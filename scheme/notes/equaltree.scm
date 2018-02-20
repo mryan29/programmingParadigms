af573dd9039e8eaac1c253e5a9ac946e92733516
Meaghan Elizabeth Ryan <mryan29@student04.cse.nd.edu>
2018-02-20 16:39:00 -0500

    Finish occur.scm, begin work on equaltree

---
+++ scheme/notes/equaltree.scm
@@ -0,0 +1,39 @@
+;; equaltree.scm
+;; Meg Ryan
+;; Feb20
+
+(define xor
+	(lambda (a b)
+		(cond
+			((eq? a b) #f)
+			(else #t)
+		)
+	)
+)
+
+(define 
+
+(define equaltree
+	(lambda (l1 l2)
+		(cond
+			((and (null? l1) (null? l2)) #t)
+			((xor (null? l1) (null? l2)) #f)
+			((and (atom? (car l1)) (atom? (car l2)))
+				(cond
+					((eq? (car l1) (car l2)) (equaltree (cdr l1) (cdr l2)))
+					(else #f)
+				)
+			)
+		)
+	)
+)
+
+;; test
+(display 
+	(equaltree 
+		'( (turkey) ((bacon)) ((cheese)) (((cheddar))) ) 
+		'( (turkey) ((bacon)) ((cheese)) (((cheddar))) )
+	)
+)
+
+(display "\n")
--- scheme/notes/occur.scm
+++ scheme/notes/occur.scm
@@ -5,34 +5,64 @@
 (load-from-path "/afs/nd.edu/user37/cmc/Public/paradigms/scheme/d7/paradigms_d7.scm")
 (use-modules (ice-9 paradigms_d7))
 
+(define atom?
+	(lambda (x)
+		(and (not (pair? x)) (not (null? x)))))
+		;; what does pair, and, not do???
 
 ;; this works WITHOUT counting inner list elements
-(define occur*
+(define occur_non
 	(lambda (a lat)
 		(cond
 			((null? lat) 0)
-			((eq? a (car lat)) (add1 (occur* a (cdr lat))))
-			(else (occur* a (cdr lat)))
+			((eq? a (car lat)) (add1 (occur_non a (cdr lat))))
+			(else (occur_non a (cdr lat)))
 		)
 	)
 )
 
-
+;; WITH RECURSING on inner lists
+(define occur*
+	(lambda (a lat)
+		(cond
+			((null? lat) 0)
+			((atom? (car lat))
+			(cond
+				((eq? (car lat) a)
+				(add1 (occur* a (cdr lat))))
+				(else (occur* a (cdr lat)))))
+			(else (+ (occur* a (car lat)) (occur* a (cdr lat)))))))
 
 
 ;; tests
+(display (occur_non 1 '(3 4 5 12 (1) 2 1 4 1)))
+(display "\n")
+
 (display (occur* 1 '(3 4 5 12 (1) 2 1 4 1)))
 (display "\n")
 
-;;(display (occur* 'castle 
-;;	'(knight sword 
-;;		(armor 
-;;			(castle 
-;;				(king) 
-;;				(horse castle) 
-;;				(((castle)))
-;;			)
-;;		)
-;;	)
-;;))
+(display (occur_non 'castle
+	'(knight sword
+		(armor
+			(castle
+				(king)
+				(horse castle)
+				(((castle)))
+			)
+		)
+	)
+))
+(display "\n")
+
+(display (occur* 'castle 
+	'(knight sword 
+		(armor 
+			(castle 
+				(king) 
+				(horse castle) 
+				(((castle)))
+			)
+		)
+	)
+))
 (display "\n")
